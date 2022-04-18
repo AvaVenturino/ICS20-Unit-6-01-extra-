524518bb63a3ad8ab106c50a9f2904aea6980970
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-18 19:09:53 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit-6-01-JS/sw.js", {
+    scope: "/ICS20-Unit-6-01-JS/",
+  })
+}
+
+/**
+ * This function calculates the volume of a sphere.
+ */
+function calculate () {
+  // input
+  const radius = parseInt(document.getElementById("radius").value)
+  // process
+  const volume = 4/3 * Math.PI * Math.pow(radius, 3)
+  // output
+  document.getElementById("volume").innerHTML =
+    "The volume is: " + volume.toFixed(2) + ' cm³' 
+}
+
